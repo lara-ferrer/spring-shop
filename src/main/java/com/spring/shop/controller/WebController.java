30e18c1251649682b8f5cc0a3eaffe1fa8bd8371
Lara <larafpics@gmail.com>
2022-01-02 20:43:05 +0100

    chore: add controllers for products and handle exceptions

--- src/main/java/com/spring/shop/controller/WebController.java
+++ src/main/java/com/spring/shop/controller/WebController.java
@@ -1,13 +1,17 @@
 package com.spring.shop.controller;
 
 import com.spring.shop.domain.Product;
+import com.spring.shop.exception.ProductNotFoundException;
 import com.spring.shop.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Set;
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
 
 @Controller
 public class WebController {
@@ -17,8 +21,25 @@ public class WebController {
 
     @RequestMapping(value = "/")
     public String index(Model model) {
-        Set<Product> products = productService.findAllProducts();
+        List<Product> products = productService.findAllProducts();
         model.addAttribute("products", products);
         return "index";
+    }
+
+    @RequestMapping(value = "/productos/{id}")
+    public String product(Model model, @PathVariable long id) throws ProductNotFoundException {
+        Product product = productService.findProduct(id);
+        model.addAttribute("product", product);
+        return "product";
+    }
+
+    @ExceptionHandler(ProductNotFoundException.class)
+    public String handleException(HttpServletRequest request, ProductNotFoundException exception) {
+        return "product_error";
+    }
+
+    @ExceptionHandler
+    public String handleException(HttpServletRequest request, Exception exception) {
+        return "error";
     }
 }
